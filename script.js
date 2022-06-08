32dfae4a6a99119ea8bc9a7171271414ce17c846
Marcin Foks <jsprogdev@gmail.com>
2022-06-08 16:10:53 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+var canvas = document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+
+ctx.fillStyle = "rgb(200,0,200)";
+ctx.fillRect(50, 40, 100, 30);
+
+ctx.fillStyle = "rgb(100,100,200)";
+ctx.fillRect(50,40,100,30);
